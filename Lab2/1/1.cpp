4b66ce74ae7d7b9a6fe0daddc9917db29ed2effc
AnnDabChan <72371498+AnnDabChan@users.noreply.github.com>
2020-11-12 14:19:32 +0200

    Add files via upload

---
+++ Lab2/1/1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    printf("Вариант - 5");
+    int a0, a1, a2;
+    char name0[15], name1[15], name2[15], name02[9], name12[9],name22[9];
+    printf("\n\nПроцессор 1 \nНазвание:");
+    scanf_s("%s", name0, 15);
+    printf("Частота:");
+    scanf_s("%d", &a0);
+    printf("Тип:");
+    scanf_s("%s", name02,9);
+    system("cls");
+
+    printf("Вариант - 5\n\n");
+    printf("Процессор 2 \nНазвание:");
+    scanf_s("%s", name1,15);
+    printf("Частота:");
+    scanf_s("%d", &a1);
+    printf("Тип:");
+    scanf_s("%s", name12,9);
+    system("cls");
+
+    printf("Вариант - 5\n\n");
+    printf("Процессор 3 \nНазвание:");
+    scanf_s("%s", name2,15);
+    printf("Частота:");
+    scanf_s("%d", &a2);
+    printf("Тип:");
+    scanf_s("%s", name22,2);
+    system("cls");
+
+    printf("Вариант - 5\n\n");
+    printf(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n");
+    printf("|Характеристики ПЭВМ                                                                |\n");
+    printf("|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|\n");
+    printf("|Процессор              |Частота(Mgz)             |RAM(Mb)            |Тип          |\n");
+    printf("|- - - - - - - - - - - -|- - - - - - - - - - - - -|- - - - - - - - - -|- - - - - - -|\n");
+    printf("|%-22s | %-23d |                   | %-11s |\n", name0, a0, name02);
+    printf("|%-22s | %-23d |                   | %-11s |\n", name1, a1, name12);
+    printf("|%-22s | %-23d |                   | %-11s |\n", name2, a2, name22);
+    printf("|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|\n");
+    printf("|Тип: C - CISC - процессор, R - RISC - процессор                                    |\n");
+    printf(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n");
+
+
+}
---
+++ Lab3/1/1.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x=44.4;  /* параметры, которые вводятся */
+    double a = 12.5, b = 1.3;  /* параметры, которые задаются в программе */
+    double t1, t2;   /* результаты */
+    double per, per2, am, bm;       /* рабочая переменная */
+
+    printf("Введите x: ");
+    scanf_s("%lf", &x);
+
+    am = -1 * a;
+    bm = -1 * b;
+
+    if (x == am or x == bm or x == -1 or tan((a * x) / 2)<0) {
+        while (x == am or x == bm or x == -1 or tan((a * x) / 2) < 0) {
+            printf("Введите другое значение x: ");
+            scanf_s("%lf", &x);
+        }
+    }
+
+    per = a - b;
+    per2 = a + x;
+    t1 = -(1 / (per*per) * ((1 / per2) + (1 / (1 + x))) + (2 / (per * per * per)) * log(per2 / (b + x)));
+
+    per = a * x * 3.14 / 180;
+    t2 = (-(1 / (2 * a)) * (cos(per) / (sin(per) * sin(per)))) - log(tan(per / 2));
+
+    printf("t1 = %lg\n", t1);
+    printf("t2 = %lg\n", t2);
+
+    //double t3, t4;
+    //t3 = tan(per / 2);
+    //t4 = -log(t3);
+    //printf("t3 = %lg\n", t3);
+    //printf("t4 = %lg\n", t4);
+}
